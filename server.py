be487f30f1f5a52bd3bc36a7b0d2ad42bbd275a9
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-06 09:49:02 +0530

    serving static files

--- server.py
+++ server.py
@@ -19,10 +19,57 @@ class RequestHandler(BaseHTTPRequestHandler):
 		</html>
 	'''
 
+	Error_Page = '''
+		<html>
+        <body>
+        <h1>Error accessing {path}</h1>
+        <p>{e}</p>
+        </body>
+        </html>
+	'''
+
 	#handle get request
 	def do_GET(self):
-		page = self.create_page()
-		self.send_page(page)
+		try:
+			#figure out what is requested exactly
+			full_path = os.getcwd() + self.path 
+
+			#if path does not exist
+			if not os.path.exists(full_path):
+				raise ServerException(" '{0}' not found".format(self.path))
+
+			#if there is a file
+			elif os.path.isfile(full_path):
+				self.handle_file(full_path)
+
+			else:
+				raise ServerException("Unknown Exception '{0}'".format(self.path))
+
+		except Exception as e:
+			self.handle_error(e)
+
+	def handle_file(self, full_path):
+		try:
+			with open(full_path, 'rb') as reader:
+				content = reader.read()
+			self.send_content(content)
+		except IOError as e:
+			e = "'{0}' cannot be read: {1}".format(self.path, e)
+			self.handle_error(e)	
+
+	#handle unknown errors
+	def handle_error(self, e):
+		content = self.Error_Page.format(path=self.path, e=e)
+		self.send_content(content, 404)
+
+	def send_content(self, content, status=200):
+		self.send_response(status)
+		self.send_header("Content-Type", "text/html")
+		self.send_header("Content-length", str(len(content)))
+		self.end_headers()
+		self.wfile.write(content.encode('utf-8'))
+
+
 
 	def create_page(self):
 		values = {
