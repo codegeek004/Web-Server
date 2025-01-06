4f78c8e09214379d18867a063dbb565080e4019b
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-06 08:47:21 +0530

    displaying values included in the http request

--- server.py
+++ server.py
@@ -7,19 +7,43 @@ class RequestHandler(BaseHTTPRequestHandler):
 	Page = '''\
 		<html>
 		<body>
-		<p>Hello web!</p>
+		<table>
+		<tr>  <td>Header</td>         <td>Value</td>          </tr>
+		<tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
+		<tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
+		<tr>  <td>Client port</td>    <td>{client_port}</td> </tr>
+		<tr>  <td>Command</td>        <td>{command}</td>      </tr>
+		<tr>  <td>Path</td>           <td>{path}</td>         </tr>
+		</table>
 		</body>
 		</html>
 	'''
 
 	#handle get request
 	def do_GET(self):
-		print('self page', self.Page)
+		page = self.create_page()
+		self.send_page(page)
+
+	def create_page(self):
+		values = {
+			'date_time' : self.date_time_string(),
+			'client_host' : self.client_address[0],
+			'client_port' : self.client_address[1],
+			'command' : self.command,
+			'path' : self.path
+		}
+		print('values', values)
+		print('page ke upar')
+		page = self.Page.format(**values)
+		print('page', page)
+		return page
+
+	def send_page(self, page):
 		self.send_response(200)
 		self.send_header("Content-Type", "text/html")
-		self.send_header("Content-length", str(len(self.Page)))
+		self.send_header("Content-length", str(len(page)))
 		self.end_headers()
-		self.wfile.write(self.Page.encode("utf-8"))
+		self.wfile.write(page.encode('utf-8'))
 
 
 if __name__ == '__main__':
